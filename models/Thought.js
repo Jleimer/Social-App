e8ba69a2944e3ec1e8b5529d253843b303011f13
Jonathan <eimerarts@gmail.com>
2020-09-26 17:54:42 -0500

    Almost set up

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: {
+
+        }
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            
+        }
+    }
+)
